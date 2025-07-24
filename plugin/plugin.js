1f950988239c5ddfd7f1f8f4822bf8d2ebfd67a7
Lucian Ruscanu <lucianruscanu@gmail.com>
2025-07-24 12:35:11 +0300

    Switch to fullscreen plugin

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -6,24 +6,10 @@ const plugin = {
     context: null,
 
     async appOption(app) {
-        // We always look for the plugin note that contains the embed button and navigate there
-        let homeNote = await app.findNote({name: "Voice notes", tags: ["system/voice-notes"]});
-        console.log(homeNote);
-        if (!homeNote) {
-            // If that note does not exist, we create it
-            let homeNoteUUID = await app.createNote("Voice notes", ["system/voice-notes"]);
-            homeNote = await app.findNote({uuid: homeNoteUUID});
-            let contents = await app.getNoteContent({uuid: homeNoteUUID});
-            console.log(contents);
-
-            // If the note exists but the embed is not inside, we insert it
-            let pluginMarkdown = `<object data="plugin://${ app.context.pluginUUID }" data-aspect-ratio="1" />\n\n# History`;
-            if (!contents.includes(pluginMarkdown)) {
-                await app.insertNoteContent({uuid: homeNote.uuid}, pluginMarkdown);
-            }
-        }
-        this.context = homeNote.uuid;
-        app.navigate(`https://www.amplenote.com/notes/${homeNote.uuid}`);
+        await app.openEmbed();
+ 
+        // The embed section isn't navigated to when calling openEmbed, but can be navigated to with:
+        await app.navigate("https://www.amplenote.com/notes/plugins/" + app.context.pluginUUID);
     },
 
     async onEmbedCall(app, ...args) {
@@ -35,13 +21,29 @@ const plugin = {
         } else if (args[0] === "insertText") {
             // Insert the transcribed text into the Voice Notes
             const textToInsert = args[1];
-            let noteHandle = await app.findNote({name: "Voice notes", tags: ["system/voice-notes"]});
+            let noteHandle = await this.ensureHomeNote(app);
+            app.navigate(`https://www.amplenote.com/notes/${noteHandle.uuid}`);
             await app.insertNoteContent(noteHandle, textToInsert, {atEnd: true});
         } else if (args[0] === "showAlert") {
             // Show an alert to the user
             await app.alert(args[1]);
         }
     },
+
+    async ensureHomeNote(app) {
+        let homeNote = await app.findNote({name: "Voice notes", tags: ["system/voice-notes"]});
+        console.log(homeNote);
+        if (!homeNote) {
+            // If that note does not exist, we create it
+            let homeNoteUUID = await app.createNote("Voice notes", ["system/voice-notes"]);
+            homeNote = await app.findNote({uuid: homeNoteUUID});
+            let contents = await app.getNoteContent({uuid: homeNoteUUID});
+            console.log(contents);
+        }
+        this.context = homeNote.uuid;
+        return homeNote;
+    },
+
     renderEmbed(app, embedType, noteUUID) {
         // Only inject the noteUUID since functions are now defined in embed/index.js
         let htmlWithModules = addWindowVariableToHtmlString(embedHTML, 'noteUUID', noteUUID);
